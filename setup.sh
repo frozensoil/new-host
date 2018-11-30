166cbfb0277beac229c617c98606d161f0241ee3
炸酱面 <yang.zhuo@hotmail.com>
2018-11-30 10:48:31 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ sed -i 's/=enforcing/=disabled/' /etc/selinux/config
 
 systemctl disable firewalld
 
+#history命令显示时间记录
+echo "HISTFILESIZE=2000" >> /etc/bashrc && echo "HISTSIZE=2000" >> /etc/bashrc && echo 'HISTTIMEFORMAT="%Y%m%d %T "'>> /etc/bashrc && export HISTTIMEFORMAT
 yum install -y epel-release
 
 yum makecache
