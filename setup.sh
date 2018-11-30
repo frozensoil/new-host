82ae50eb7c7dd2c996be6fab815e55b8153b541c
炸酱面 <yang.zhuo@hotmail.com>
2018-11-30 09:52:11 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ sed -i 's/=enforcing/=disabled/' /etc/selinux/config
 
 systemctl disable firewalld
 
+yum install -y epel-release
+
 yum makecache
 
 yum install -y net-tools,lrzsz,pigz,vim,bash-completion,gcc,gcc--c++
