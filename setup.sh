7b8ff2bce64d38c4aee5f9874782986e72cc27b2
炸酱面 <yang.zhuo@hotmail.com>
2018-12-03 11:45:35 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ sed -i 's/=enforcing/=disabled/' /etc/selinux/config
 
 systemctl disable firewalld
 
+sed 's/# vim/vim/g' /etc/bashrc
+
 #history命令显示时间记录
 echo "HISTFILESIZE=2000" >> /etc/bashrc && echo "HISTSIZE=2000" >> /etc/bashrc && echo 'HISTTIMEFORMAT="%Y%m%d %T "'>> /etc/bashrc && export HISTTIMEFORMAT
 yum install -y epel-release
@@ -12,6 +14,6 @@ yum makecache
 
 yum install -y net-tools lrzsz pigz vim bash-completion gcc gcc--c++
 
-echo "set ts=4" >> /etc/vimrc
+#echo "set ts=4" >> /etc/vimrc
 
 echo "set hlsearch" >> /etc/vimrc
